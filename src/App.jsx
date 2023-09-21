aa7b1df220d49e681a22c204847712db9b69209f
Mohit <mohit2412gupta@gmail.com>
2023-09-22 01:09:41 +0530

    Completing task feature added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import {
 } from "@heroicons/react/24/solid";
 
 import "./App.css";
+import { list } from "postcss";
 
 function App() {
   const {
@@ -29,15 +30,32 @@ function App() {
     setTask({ title: "", desc: "", uid: uuid() });
   };
 
-  const deleteHandler = (id) => {
-    setTaskList((prevTaskList) => prevTaskList.filter((t) => t.uid !== id));
+  const deleteHandler = (id, List) => {
+    if (List === taskList) {
+      setTaskList((prevTaskList) => prevTaskList.filter((t) => t.uid !== id));
+    } else if (List === completeTaskList) {
+      setCompleteTaskList((prevCompleteTaskList) =>
+        prevCompleteTaskList.filter((t) => t.uid !== id)
+      );
+    }
+  };
+
+  const completeHandler = (id, taskList) => {
+    // remove elem with that id from taskList
+    const completeTask = taskList.filter((t) => t.uid === id);
+    deleteHandler(id, taskList);
+    // add that elem to completeTaskList
+    setCompleteTaskList((prevCompleteTaskList) => [
+      ...prevCompleteTaskList,
+      completeTask[0],
+    ]);
   };
 
   const addTask = (task) => {
     setTaskList((prevTaskList) => [...prevTaskList, task]);
   };
 
-  const renderTaskList = (taskList, deleteTask) => {
+  const renderTaskList = (taskList, onDelete, onComplete) => {
     if (taskList.length > 0) {
       {
         return (
@@ -60,12 +78,15 @@ function App() {
                   <button className="bg-yellow-300  cursor-pointer rounded-lg  h-10 w-10 p-2 mx-2 border-2 border-yellow-400  ">
                     <PencilIcon height={22} width={22} />
                   </button>
-                  <button className="bg-green-300 h-10 w-10 p-2 mx-2 border-2 cursor-pointer rounded-lg border-green-400 ">
+                  <button
+                    className="bg-green-300 h-10 w-10 p-2 mx-2 border-2 cursor-pointer rounded-lg border-green-400 "
+                    onClick={() => onComplete(task.uid, taskList)}
+                  >
                     <CheckBadgeIcon height={22} width={22} />
                   </button>
                   <button
-                    onClick={() => deleteTask(task.uid)}
                     className="bg-red-300  h-10 w-10 p-2 mx-2 border-2 border-red-400 cursor-pointer rounded-lg"
+                    onClick={() => onDelete(task.uid, taskList)}
                   >
                     <TrashIcon height={22} width={22} />
                   </button>
@@ -84,7 +105,45 @@ function App() {
     }
   };
 
-  const renderCompleteTaskList = () => {};
+  const renderCompleteTaskList = (completeTaskList, onDelete) => {
+    if (completeTaskList.length > 0) {
+      return (
+        <ul>
+          {completeTaskList.map((compTask) => {
+            return (
+              <li
+                className=" bg-green-300 border-2 border-green-400 h-20 my-2.5 mx-4 rounded-lg py-1 px-2 flex justify-between"
+                key={compTask.uid}
+              >
+                <div>
+                  <div className=" m-1 text-xl font-bold  text-[hsl(0,100%,12%)] ">
+                    {compTask.title}
+                  </div>
+                  <div className=" m-1 text-l font-semibold  text-[hsl(0,100%,12%)]">
+                    {compTask.desc}
+                  </div>
+                </div>
+                <div className=" flex justify-center items-center ">
+                  <button
+                    className="bg-red-300  h-10 w-10 p-2 mx-2 border-2 border-red-400 "
+                    onClick={() => onDelete(compTask.uid, completeTaskList)}
+                  >
+                    <TrashIcon height={22} width={22} />
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      return (
+        <h1 className="text-zinc-500 p-4  text-2xl font-bold text-start">
+          No task Completed...{" "}
+        </h1>
+      );
+    }
+  };
 
   return (
     <>
@@ -100,9 +159,9 @@ function App() {
             placeholder="Enter title here ... "
             required
             autoFocus
+            maxLength={30}
             {...register("title", {
               required: "Title cannot be empty",
-              maxLength: "it cannot this long",
             })}
             value={task.title}
             onChange={(e) => setTask({ ...task, title: e.target.value })}
@@ -112,9 +171,9 @@ function App() {
             type="text"
             name="title"
             placeholder="Enter Description here ... "
+            maxLength={60}
             {...register("desc", {
               required: "Description cannot be empty",
-              maxLength: "it cannot this long",
             })}
             value={task.desc}
             onChange={(e) => setTask({ ...task, desc: e.target.value })}
@@ -145,28 +204,14 @@ function App() {
           <h1 className="text-zinc-500 p-4  text-3xl font-extrabold text-start">
             Task List
           </h1>
-          {renderTaskList(taskList, deleteHandler)}
+          {renderTaskList(taskList, deleteHandler, completeHandler)}
         </div>
 
         <div className="bg-slate-200 border-2 border-slate-400 rounded-xl h-fit w-1/3 py-2 px-1">
           <h1 className="text-zinc-500 p-4  text-2xl font-bold text-start">
             Completed List
           </h1>
-          {/* <div className=" bg-green-300 border-2 border-green-400 h-20 my-2.5 mx-4 rounded-lg py-1 px-2 flex justify-between">
-            <div>
-              <div className=" m-1 text-xl font-bold  text-[hsl(0,100%,12%)] ">
-                Title
-              </div>
-              <div className=" m-1 text-l font-semibold  text-[hsl(0,100%,12%)]">
-                Description
-              </div>
-            </div>
-            <div className=" flex justify-center items-center ">
-              <button className="bg-red-300  h-10 w-10 p-2 mx-2 border-2 border-red-400 ">
-                <TrashIcon height={22} width={22} />
-              </button>
-            </div>
-          </div> */}
+          {renderCompleteTaskList(completeTaskList)}
         </div>
       </div>
     </>
