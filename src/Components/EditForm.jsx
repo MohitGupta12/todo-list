60340ccecd3b55e6d9827a786e0a85eafab4abe1
Mohit <mohit2412gupta@gmail.com>
2024-01-29 02:43:30 +0530

    DE 1

--- src/Components/EditForm.jsx
+++ src/Components/EditForm.jsx
@@ -34,7 +34,7 @@ const EditForm = ({ task, onUpdate, IsEditing, onClose }) => {
 
   return (
     <>
-      <div className=" ease-in-out duration-600 bg-[rgba(0,0,0,0.5)] fixed  h-full w-full flex  items- justify-evenly">
+      <div className=" ease-in-out duration-600 bg-[rgba(0,0,0,.7)] fixed  h-full w-full flex  items- justify-evenly">
         <form className=" bg-yellow-200 m-2  px-8 py-6 rounded-2xl  flex fixed top-[35%] left-[35%]  flex-col max-w-[800px] border-4 border-yellow-300  shadow-2xl shadow-yellow-700/40 hover:shadow-yellow-400/50 transition-all ease-in-out duration-300">
           <div className="flex flex-col">
             <input
@@ -51,7 +51,7 @@ const EditForm = ({ task, onUpdate, IsEditing, onClose }) => {
               onChange={(e) =>
                 setEditTask({ ...editTask, title: e.target.value })
               }
-              className=" text-black bg-slate-100 border-stone-500/40 border-2 rounded-lg m-2 px-5 py-2 "
+              className="px-5 py-2 m-2 text-black border-2 rounded-lg bg-slate-100 border-stone-500/40"
             />
             <input
               type="text"
@@ -65,22 +65,22 @@ const EditForm = ({ task, onUpdate, IsEditing, onClose }) => {
               onChange={(e) =>
                 setEditTask({ ...editTask, desc: e.target.value })
               }
-              className=" text-black bg-slate-100 border-stone-500/40 border-2 rounded-lg m-2 px-5 py-2 "
+              className="px-5 py-2 m-2 text-black border-2 rounded-lg bg-slate-100 border-stone-500/40"
             />
             {errors?.title ? (
-              <small className="text-yellow-600 pt-2  text-xl font-bold text-center  ">
+              <small className="pt-2 text-xl font-bold text-center text-yellow-600 ">
                 {errors.title.message}
               </small>
             ) : null}
 
             {!errors?.title && errors?.desc ? (
-              <small className="text-yellow-600 text-xl font-bold text-center  ">
+              <small className="text-xl font-bold text-center text-yellow-600 ">
                 {errors.desc.message}
               </small>
             ) : null}
             <div className="flex">
               <button
-                className="bg-green-300 border-green-500/40  shadow-md shadow-green-700/60 transition ease-in-out duration-300 hover:shadow-green-300/60 border-2 mb-2 mt-4 mx-12 px-2 py-2 rounded-md flex justify-center items-center"
+                className="flex items-center justify-center px-2 py-2 mx-12 mt-4 mb-2 transition duration-300 ease-in-out bg-green-300 border-2 rounded-md shadow-md border-green-500/40 shadow-green-700/60 hover:shadow-green-300/60"
                 onClick={handleSubmit(submitHandler)}
               >
                 <CheckIcon
@@ -92,7 +92,7 @@ const EditForm = ({ task, onUpdate, IsEditing, onClose }) => {
                 Update
               </button>
               <button
-                className="bg-red-300  border-red-500/40 shadow-md shadow-red-700/60 transition ease-in-out duration-300 hover:shadow-red-300/60 border-2 mb-2 mt-4 mx-12 px-2 py-2 rounded-md flex justify-center items-center"
+                className="flex items-center justify-center px-2 py-2 mx-12 mt-4 mb-2 transition duration-300 ease-in-out bg-red-300 border-2 rounded-md shadow-md border-red-500/40 shadow-red-700/60 hover:shadow-red-300/60"
                 onClick={onClose}
               >
                 <XMarkIcon
